e7fa55c4138dbb7b11c0a4ca5921e1aaa0cace02
omarPerez1987 <omar.perez.dev@gmail.com>
2024-04-04 12:53:26 +0200

    feat: añado pruebas rutas con grafo dirigido

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const grafo = {
+const directedGraph = {
   a: ["b", "c"],
   b: ["d"],
   c: ["e"],
@@ -6,6 +6,7 @@ const grafo = {
   e: [],
   f: ["e"],
 };
+
 
 //Depth-First Search, DFS
 function dfs(graph, source) {
@@ -15,33 +16,73 @@ function dfs(graph, source) {
     console.log("Esta corriendo", current);
     for (let neighbor of graph[current]) {
       stack.push(neighbor);
-      console.log("busco al vecino",neighbor)
+      console.log("busco al vecino", neighbor);
     }
   }
-  return console.log(stack)
+  return console.log("finaliza con:", stack);
 }
 
-// dfs(grafo, "a")
+// dfs(directedGraph, "a")
 
 function recursiveDFS(graph, source) {
   console.log("Esta corriendo", source);
-  for(let neighbor of graph[source]) {
-    recursiveDFS(graph, neighbor)
+  for (let neighbor of graph[source]) {
+    recursiveDFS(graph, neighbor);
   }
 }
-// recursiveDFS(grafo, "a")
+// recursiveDFS(directedGraph, "a")
 
 //Breadth-First Search, BFS
-function bfs (graph, source) {
-  const queue = [source]
-  while(queue.length > 0) {
-    const current = queue.shift()
+function bfs(graph, source) {
+  const queue = [source];
+  while (queue.length > 0) {
+    const current = queue.shift();
     console.log("Esta corriendo", current);
-    for(let neighbor of graph[current]) {
-      queue.push(neighbor)
+    for (let neighbor of graph[current]) {
+      queue.push(neighbor);
     }
   }
-  return console.log(queue)
+  return console.log(queue);
 }
 
-bfs(grafo, "f")
+// bfs(directedGraph, "f");
+
+
+
+// /* Existe Ruta? */******************************************* */
+
+const directedGraph2 = {
+  f: ["g", "i"],
+  g: ["h"],
+  h: [],
+  i: ["g", "k"],
+  j: ["i"],
+  k: [],
+};
+
+
+function hasPath(graph, source, destination) {
+  const queue = [source];
+  while (queue.length > 0) {
+    const current = queue.shift();
+    if (current === destination)
+      return console.log(`result ${source} and ${destination} is:`, true);
+    for (let neighbor of graph[current]) {
+      queue.push(neighbor);
+    }
+  }
+  return console.log(`result ${source} and ${destination} is:`, false);
+}
+
+// hasPath(directedGraph2, "f", "k");
+
+function recursiveHasPath(graph, source, destination) {
+  if (source === destination) return true;
+  for (let neighbor of graph[source]) {
+    if (recursiveHasPath(graph, neighbor, destination)) return true;
+  }
+  return false;
+}
+
+const result = recursiveHasPath(directedGraph2, "f", "k");
+console.log(result);
